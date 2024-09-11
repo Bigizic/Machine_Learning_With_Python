d927c4cc8b1e3ebb9f5e781b084dd7ce4f03e0b0
Bigizic <olalekanisaac75@gmail.com>
2024-09-11 11:47:26 +0100

    add complete linear regression solution

--- final_project/linear_regression.py
+++ final_project/linear_regression.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
-from sklearn.metrics import jaccard_score
+from sklearn.metrics import jaccard_score, r2_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import log_loss
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -56,11 +56,33 @@ df_sydney_processed = df_sydney_processed.astype(float)
 features = df_sydney_processed.drop(columns='RainTomorrow', axis=1)
 Y = df_sydney_processed['RainTomorrow']
 
-# Linear regression solution
+# Train and Test Split
 X_train, X_test, Y_train, Y_test = train_test_split(features, Y,
                                                     test_size=0.2,
                                                     random_state=10)
+# Linear Regression Model Training
+LinearReg = LinearRegression()
+LinearReg.fit(X_train, Y_train)
 
+predictions = LinearReg.predict(X_test)
 
+# Linear Mean squared error, mean absolute error and R2 score
+LinearRegression_MAE = np.mean(np.absolute(predictions - Y_test))
+LinearRegression_MSE = np.mean((predictions - Y_test) ** 2)
+LinearRegression_R2 = r2_score(Y_test, predictions)
+
+print("MAE %.2f: "% LinearRegression_MAE)
+print("MSE %.2f: "% LinearRegression_MSE)
+print("R2 SCORE %.2f: "% LinearRegression_R2)
+
+# Show the MAE, MSE, and R2 in a tabular format using data frame
+# for the linear model
+report = {"Metrics": ["MAE", "MSE", "R2 Score"],
+          "Result": [LinearRegression_MAE,
+                     LinearRegression_MSE,
+                     LinearRegression_R2]
+          }
+
+pd.DataFrame(report)
 
 warnings.warn = warn
